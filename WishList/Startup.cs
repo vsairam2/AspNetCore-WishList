8278b2cf5c0734265c8a792884290403b641b6af
sairam <sairamvarma00@gmail.com>
2022-07-11 07:10:19 +0000

    modu

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,17 +23,19 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+           
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-            }
+                
+            app.UseDeveloperExceptionPage();
+        }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
 
-        
-            app.UseMvcWithDefaultRoute();
+    app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
